72176ab25fd81fd0493e793fc4a9b7c776ba8e69
Gregory Desfour <gregory.desfour@accedo.tv>
2017-10-19 15:59:15 +1300

    :white_check_mark: Fix tests

--- test/specs/express.spec.js
+++ test/specs/express.spec.js
@@ -2,7 +2,7 @@
 
 const chai = require('chai');
 const sinon = require('sinon');
-const appgridMiddleware = require('../../src/index');
+const middleware = require('../../src/index');
 
 const should = chai.should();
 
@@ -23,94 +23,91 @@ const buildResponse = () => ({
 });
 
 describe('The Express middleware, with basic options', () => {
-  const mw = appgridMiddleware({
+  const oneMw = middleware({
     appKey,
     gid: 'myGID',
   });
 
   it('exposes a function', () => {
-    appgridMiddleware.should.be.a('function');
+    middleware.should.be.a('function');
   });
 
   it('also exposes some defaults as properties of that function', () => {
-    appgridMiddleware.defaultOnDeviceIdGenerated.should.be.a('function');
-    appgridMiddleware.defaultGetRequestInfo.should.be.a('function');
-    appgridMiddleware.defaultOnSessionKeyChanged.should.be.a('function');
-    appgridMiddleware.COOKIE_DEVICE_ID.should.be.a('string');
-    appgridMiddleware.COOKIE_SESSION_KEY.should.be.a('string');
+    middleware.defaultOnDeviceIdGenerated.should.be.a('function');
+    middleware.defaultGetRequestInfo.should.be.a('function');
+    middleware.defaultOnSessionKeyChanged.should.be.a('function');
+    middleware.COOKIE_DEVICE_ID.should.be.a('string');
+    middleware.COOKIE_SESSION_KEY.should.be.a('string');
   });
 
   it('should call the next handler in the Express pipeline', () => {
     const next = sinon.spy();
-    mw(buildRequest(), buildResponse(), next);
+    oneMw(buildRequest(), buildResponse(), next);
     next.calledOnce.should.be.true;
   });
 
-  it('should attach a configured locals.appgridClient response prop as per the parameters and cookies', () => {
+  it('should attach a configured locals.accedoOneClient response prop as per the parameters and cookies', () => {
     const response = buildResponse();
     const cookies = {
-      [appgridMiddleware.COOKIE_DEVICE_ID]: 'gregTestingSDK',
-      [appgridMiddleware.COOKIE_SESSION_KEY]: 'gregSessionKey',
+      [middleware.COOKIE_DEVICE_ID]: 'gregTestingSDK',
+      [middleware.COOKIE_SESSION_KEY]: 'gregSessionKey',
     };
-    mw(buildRequest(cookies), response, NOOP);
-    const { appgridClient } = response.locals;
-    appgridClient.should.be.an('object');
-    appgridClient.props.config.appKey.should.equal(appKey);
-    appgridClient.props.config.gid.should.equal('myGID');
-    appgridClient.props.config.deviceId.should.equal(
-      cookies[appgridMiddleware.COOKIE_DEVICE_ID]
+    oneMw(buildRequest(cookies), response, NOOP);
+    const { accedoOneClient } = response.locals;
+    accedoOneClient.should.be.an('object');
+    accedoOneClient.props.config.appKey.should.equal(appKey);
+    accedoOneClient.props.config.gid.should.equal('myGID');
+    accedoOneClient.props.config.deviceId.should.equal(
+      cookies[middleware.COOKIE_DEVICE_ID]
     );
-    appgridClient.props.config.sessionKey.should.equal(
-      cookies[appgridMiddleware.COOKIE_SESSION_KEY]
+    accedoOneClient.props.config.sessionKey.should.equal(
+      cookies[middleware.COOKIE_SESSION_KEY]
     );
   });
 
   it('should use the gid from the request if any was found there (even if a gid was set in the middleware config)', () => {
     const response = buildResponse();
     const cookies = {
-      [appgridMiddleware.COOKIE_DEVICE_ID]: 'gregTestingSDK',
-      [appgridMiddleware.COOKIE_SESSION_KEY]: 'gregSessionKey',
+      [middleware.COOKIE_DEVICE_ID]: 'gregTestingSDK',
+      [middleware.COOKIE_SESSION_KEY]: 'gregSessionKey',
     };
     const REQUEST_GID = 'a_cool_gid';
     const query = {
       gid: REQUEST_GID,
     };
-    mw(buildRequest(cookies, query), response, NOOP);
-    const { appgridClient } = response.locals;
-    appgridClient.props.config.gid.should.equal(REQUEST_GID);
+    oneMw(buildRequest(cookies, query), response, NOOP);
+    const { accedoOneClient } = response.locals;
+    accedoOneClient.props.config.gid.should.equal(REQUEST_GID);
   });
 
   describe('when no deviceId or sessionKey is found in the request cookies', () => {
-    const mw = appgridMiddleware({
+    const mw = middleware({
       // eslint-disable-line no-shadow
       appKey,
     });
     const response = buildResponse();
     mw(buildRequest(), response, NOOP);
-    const { appgridClient } = response.locals;
+    const { accedoOneClient } = response.locals;
 
     it('generates the deviceId on invocation and sets the deviceId response cookie', () => {
-      const { deviceId } = appgridClient.props.config;
+      const { deviceId } = accedoOneClient.props.config;
       deviceId.should.be.a('string');
-      response.cookie.calledWith(appgridMiddleware.COOKIE_DEVICE_ID, deviceId)
-        .should.be.true;
+      response.cookie.calledWith(middleware.COOKIE_DEVICE_ID, deviceId).should
+        .be.true;
     });
 
     it('does not create a session if not needed', () => {
-      const { sessionKey } = appgridClient.props.config;
+      const { sessionKey } = accedoOneClient.props.config;
       should.equal(false, !!sessionKey);
-      response.cookie.calledWith(appgridMiddleware.COOKIE_SESSION_KEY).should.be
-        .false;
+      response.cookie.calledWith(middleware.COOKIE_SESSION_KEY).should.be.false;
     });
 
     it('when a sessionKey is (re)created, sets the sessionKey response cookie', () => {
-      return appgridClient.getApplicationStatus().then(() => {
-        const { sessionKey } = appgridClient.props.config;
+      return accedoOneClient.getApplicationStatus().then(() => {
+        const { sessionKey } = accedoOneClient.props.config;
         sessionKey.should.be.a('string');
-        response.cookie.calledWith(
-          appgridMiddleware.COOKIE_SESSION_KEY,
-          sessionKey
-        ).should.be.true;
+        response.cookie.calledWith(middleware.COOKIE_SESSION_KEY, sessionKey)
+          .should.be.true;
       });
     });
   });
@@ -118,7 +115,7 @@ describe('The Express middleware, with basic options', () => {
 
 describe('The Express middleware used with the getRequestInfo option', () => {
   describe('when getRequestInfo returns en empty object', () => {
-    const mw = appgridMiddleware({
+    const mw = middleware({
       appKey,
       gid: 'myGID',
       getRequestInfo: () => ({}),
@@ -127,27 +124,27 @@ describe('The Express middleware used with the getRequestInfo option', () => {
     it('should ignore the cookies used by the default getRequestInfo', () => {
       const response = buildResponse();
       const cookies = {
-        [appgridMiddleware.COOKIE_DEVICE_ID]: 'gregTestingSDK',
-        [appgridMiddleware.COOKIE_SESSION_KEY]: 'gregSessionKey',
+        [middleware.COOKIE_DEVICE_ID]: 'gregTestingSDK',
+        [middleware.COOKIE_SESSION_KEY]: 'gregSessionKey',
       };
       mw(buildRequest(cookies), response, NOOP);
-      const { appgridClient } = response.locals;
-      appgridClient.should.be.an('object');
+      const { accedoOneClient } = response.locals;
+      accedoOneClient.should.be.an('object');
       // auto-generated deviceId
-      appgridClient.props.config.deviceId.should.be.a.string;
-      appgridClient.props.config.deviceId.should.not.equal(
-        cookies[appgridMiddleware.COOKIE_DEVICE_ID]
+      accedoOneClient.props.config.deviceId.should.be.a.string;
+      accedoOneClient.props.config.deviceId.should.not.equal(
+        cookies[middleware.COOKIE_DEVICE_ID]
       );
       // no sessionKey
-      should.equal(false, !!appgridClient.props.config.sessionKey);
+      should.equal(false, !!accedoOneClient.props.config.sessionKey);
       // gid is set as per the param
-      appgridClient.props.config.gid.should.equal('myGID');
+      accedoOneClient.props.config.gid.should.equal('myGID');
     });
   });
 
   describe('when getRequestInfo returns an object with sessionKey', () => {
     const sessionKey = 'MySessionKey';
-    const mw = appgridMiddleware({
+    const mw = middleware({
       appKey,
       gid: 'myGID',
       getRequestInfo: () => ({
@@ -158,17 +155,17 @@ describe('The Express middleware used with the getRequestInfo option', () => {
     it('should ignore the cookies used by the default getRequestInfo, NOT use the given sessionKey because the deviceId is missing', () => {
       const response = buildResponse();
       const cookies = {
-        [appgridMiddleware.COOKIE_DEVICE_ID]: 'gregTestingSDK',
-        [appgridMiddleware.COOKIE_SESSION_KEY]: 'gregSessionKey',
+        [middleware.COOKIE_DEVICE_ID]: 'gregTestingSDK',
+        [middleware.COOKIE_SESSION_KEY]: 'gregSessionKey',
       };
       mw(buildRequest(cookies), response, NOOP);
-      const { appgridClient } = response.locals;
+      const { accedoOneClient } = response.locals;
       // deviceId not as per the cookie
-      appgridClient.props.config.deviceId.should.not.equal(
-        cookies[appgridMiddleware.COOKIE_DEVICE_ID]
+      accedoOneClient.props.config.deviceId.should.not.equal(
+        cookies[middleware.COOKIE_DEVICE_ID]
       );
       // sessionKey NOT set because it's reset when there's no deviceId reused
-      should.equal(false, !!appgridClient.props.config.sessionKey);
+      should.equal(false, !!accedoOneClient.props.config.sessionKey);
     });
   });
 
@@ -176,7 +173,7 @@ describe('The Express middleware used with the getRequestInfo option', () => {
     const sessionKey = 'MyOtherSessionKey';
     const deviceId = 'ABoringDeviceId';
     const gid = 'myGID';
-    const mw = appgridMiddleware({
+    const mw = middleware({
       appKey,
       gid,
       getRequestInfo: () => ({
@@ -187,21 +184,21 @@ describe('The Express middleware used with the getRequestInfo option', () => {
 
     const response = buildResponse();
     const cookies = {
-      [appgridMiddleware.COOKIE_DEVICE_ID]: 'gregTestingSDK',
-      [appgridMiddleware.COOKIE_SESSION_KEY]: 'gregSessionKey',
+      [middleware.COOKIE_DEVICE_ID]: 'gregTestingSDK',
+      [middleware.COOKIE_SESSION_KEY]: 'gregSessionKey',
     };
     mw(buildRequest(cookies), response, NOOP);
-    const { appgridClient } = response.locals;
+    const { accedoOneClient } = response.locals;
 
     it('should ignore the cookies used by the default getRequestInfo, use the given sessionKey', () => {
       // deviceId as given (not as per the cookie)
-      appgridClient.props.config.deviceId.should.equal(deviceId);
+      accedoOneClient.props.config.deviceId.should.equal(deviceId);
       // sessionKey as given because a deviceId was also given
-      appgridClient.props.config.sessionKey.should.equal(sessionKey);
+      accedoOneClient.props.config.sessionKey.should.equal(sessionKey);
     });
 
     it('should keep the gid given in the middleware constructor', () => {
-      appgridClient.props.config.gid.should.equal(gid);
+      accedoOneClient.props.config.gid.should.equal(gid);
     });
   });
 });
